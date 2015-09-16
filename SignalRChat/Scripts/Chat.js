84170a826983f5c0adcac9c7f6948e792771449d
Van Kichline <vkichline@hotmail.com>
2015-09-16 10:54:08 -0700

    Minor improvements

---
+++ SignalRChat/Scripts/Chat.js
@@ -0,0 +1,50 @@
+﻿$(function () {
+    // Set text field so that an enter presses the button
+    $('#message').keyup(function (evt) {
+        if (evt.which === 13) {
+            // Enter key was pressed
+            evt.preventDefault();
+            $('#sendmessage').click();
+        }
+    });
+
+    // Declare a proxy to reference the hub.
+    var chat = $.connection.chatHub;
+    // Create a function that the hub can call to broadcast messages.
+    chat.client.broadcastMessage = function (name, message) {
+        // Html encode display name and message.
+        var encodedName = $('<div />').text(name).html();
+        var encodedMsg = $('<div />').text(message).html();
+        // Add the message to the page.
+        $('#discussion').prepend('<li><strong>' + encodedName
+            + '</strong>:&nbsp;&nbsp;' + getTime() + '&nbsp;&nbsp;' + encodedMsg + '</li>');
+    };
+    // Get the user name and store it to prepend to messages.
+    $('#displayname').val(prompt('Enter your name:', ''));
+    // Set initial focus to message input box.
+    $('#message').focus();
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        $('#sendmessage').click(function () {
+            // Call the Send method on the hub.
+            chat.server.send($('#displayname').val(), $('#message').val());
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+    });
+
+    function getTime() {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+
+        if (minutes < 10)
+            minutes = "0" + minutes;
+
+        if (seconds < 10)
+            seconds = "0" + seconds;
+
+        return hours + ":" + minutes + ":" + seconds;
+    }
+});
